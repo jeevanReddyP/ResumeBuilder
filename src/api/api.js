68c548c4ec507ce2ed39b3e7b7112ea764678b4b
JeevanReddy <jeevanjgfr@gmail.com>
2025-10-28 16:50:57 +0530

    api update

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
   import axios from "axios";
 
   const API = axios.create({
-    baseURL: "https://resumebuilderbackend1.onrender.com/",
+    baseURL: "https://resumebuilderbackend1.onrender.com/api",
   });
 
   API.interceptors.request.use((req) => {
